506dfe07d115a458c66057e0eb4587e26444bdf7
gitswati-27 <swatik2706@gmail.com>
2025-08-17 11:01:47 +0530

    delete and complete task feature added

--- app.js
+++ app.js
@@ -10,14 +10,39 @@ function addTask(task){
         return;
     } 
     let li = document.createElement("li");
-    li.innerText = task;
+    let textSpan = document.createElement("span"); 
+    textSpan.innerText = task;
+    li.appendChild(textSpan); 
     lis.push(task);
-    ul.append(li);
+    ul.appendChild(li);
+    let checkbox = document.createElement("input");
+    checkbox.setAttribute("type", "checkbox");
+    // console.dir(checkbox);
+    li.append(checkbox);
+    checkbox.addEventListener("change", function(){
+        if(checkbox.checked){
+            textSpan.classList.add('strike');
+        }
+        else{
+           textSpan.classList.remove('strike');
+        }
+    });
+    let i = document.createElement("i");
+    i.setAttribute("class", "fa-solid fa-trash-can");
+    i.setAttribute("type", "button");
+    i.addEventListener("click", function(){
+        ul.removeChild(li);
+        let val = li.innerText;
+        let x = lis.indexOf(val);
+        lis.splice(x,1);
+    });
+    li.append(i);
 }
 
 btn.addEventListener("click", function(){
     let inp = document.querySelector("input");
     addTask(inp.value);
+    inp.value = "";
 });
 
 inp.addEventListener("keypress",function(event){
