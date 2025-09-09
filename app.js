e814a29e3d2b44fd932c334fb4e9df76853665ad
gitswati-27 <swatik2706@gmail.com>
2025-09-09 19:33:02 +0530

    style and layout improved

--- app.js
+++ app.js
@@ -1,53 +1,94 @@
 let inp = document.querySelector("input");
-let btn = document.querySelector("button");
+let addBtn = document.querySelector("button");
 let ul = document.querySelector("ul");
 let lis = [];
 
-function addTask(task){
-    if(task=="") return;
-    else if(lis.includes(task)){
+let taskCount = document.querySelector("#task-count");
+let clearAllBtn = document.querySelector("#clear-all");
+let filterPending = document.querySelector("#filter-pending");
+let filterCompleted = document.querySelector("#filter-completed");
+let showAll = document.querySelector("#show-all");
+
+function updateCount() {
+    taskCount.innerText = `${lis.length} task${lis.length !== 1 ? "s" : ""} total`;
+}
+
+function addTask(task) {
+    if (task === "") return;
+    else if (lis.includes(task)) {
         alert("Task already added!");
         return;
-    } 
+    }
+
     let li = document.createElement("li");
-    let textSpan = document.createElement("span"); 
+    let textSpan = document.createElement("span");
     textSpan.innerText = task;
-    li.appendChild(textSpan); 
+    li.appendChild(textSpan);
     lis.push(task);
     ul.appendChild(li);
+
     let checkbox = document.createElement("input");
     checkbox.setAttribute("type", "checkbox");
-    // console.dir(checkbox);
-    li.append(checkbox);
-    checkbox.addEventListener("change", function(){
-        if(checkbox.checked){
-            textSpan.classList.add('strike');
+    li.prepend(checkbox);
+
+    checkbox.addEventListener("change", function () {
+        if (checkbox.checked) {
+            textSpan.classList.add("strike");
+        } else {
+            textSpan.classList.remove("strike");
         }
-        else{
-           textSpan.classList.remove('strike');
-        }
+        applyFilter(); // update view when filtering
     });
+
     let i = document.createElement("i");
     i.setAttribute("class", "fa-solid fa-trash-can");
     i.setAttribute("type", "button");
-    i.addEventListener("click", function(){
+    i.addEventListener("click", function () {
         ul.removeChild(li);
-        let val = li.innerText;
+        let val = textSpan.innerText; // use only task text
         let x = lis.indexOf(val);
-        lis.splice(x,1);
+        if (x > -1) lis.splice(x, 1);
+        updateCount();
     });
     li.append(i);
+
+    updateCount();
 }
 
-btn.addEventListener("click", function(){
-    let inp = document.querySelector("input");
-    addTask(inp.value);
+addBtn.addEventListener("click", function () {
+    addTask(inp.value.trim());
     inp.value = "";
 });
 
-inp.addEventListener("keypress",function(event){
-    if(event.code=='Enter'){
-        addTask(inp.value);
+inp.addEventListener("keypress", function (event) {
+    if (event.code === "Enter") {
+        addTask(inp.value.trim());
         inp.value = "";
     }
 });
+
+clearAllBtn.addEventListener("click", function () {
+    ul.innerHTML = "";
+    lis = [];
+    updateCount();
+});
+
+function applyFilter(type = "all") {
+    let items = ul.querySelectorAll("li");
+    items.forEach((li) => {
+        let checkbox = li.querySelector("input[type='checkbox']");
+        if (type === "pending" && checkbox.checked) {
+            li.style.display = "none";
+        } else if (type === "completed" && !checkbox.checked) {
+            li.style.display = "none";
+        } else {
+            li.style.display = "flex";
+        }
+    });
+}
+
+filterPending.addEventListener("click", () => applyFilter("pending"));
+filterCompleted.addEventListener("click", () => applyFilter("completed"));
+showAll.addEventListener("click", () => applyFilter("all"));
+
+updateCount();
